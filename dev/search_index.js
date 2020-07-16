0dc46051051800f78dacb3aa99ca1aad95dfd80c
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-07-16 12:17:58 +0000

    build based on f8262e3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CoolTensors","category":"page"},{"location":"#CoolTensors","page":"Home","title":"CoolTensors","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CoolTensors]","category":"page"}]
+}
