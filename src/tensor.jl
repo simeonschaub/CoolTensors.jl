f0459fed4b06c51ec3f49bd0bee922f878ad09c9
Simeon Schaub <simeondavidschaub99@gmail.com>
2020-07-26 01:14:43 +0200

    wip

--- src/tensor.jl
+++ src/tensor.jl
@@ -81,3 +81,34 @@ for (typed_f, f) in [
     @eval Base.$typed_f(ipos::IndexPos, args...) = ipos(Base.$f(args...))
 end
 Base.getindex(::IndexPos{0}, x) = T""(fill(x))
+
+struct TAxis{T,ipos,R<:AbstractUnitRange{T}} <: AbstractUnitRange{T}
+    parent::R
+end
+TAxis{T,ipos}(r::R) where {T,ipos,R} = TAxis{T,ipos,R}(r)
+index_pos(::TAxis{<:Any,ipos}) where {ipos} = ipos
+Base.parent(t::TAxis) = t.parent
+Base.first(t::TAxis) = first(t.parent)
+Base.last(t::TAxis) = last(t.parent)
+function Base.show(io::IO, t::TAxis{T,ipos}) where {T,ipos}
+    print(io, "TAxis{", T, ",", ipos, "}(")
+    show(io, t.parent)
+    print(io, ")")
+end
+
+function Base.axes(t::Tensor{<:Any,N,ipos}) where {N,ipos}
+    ax = axes(t.parent)
+    ntuple(i -> TAxis{Int,IndexPos{1}(UInt(position(ipos, i)))}(ax[i]), N)
+end
+#Base.UnitRange(t::TAxis) = UnitRange(t.parent)
+#Base.UnitRange{Int}(t::Tensor) = UnitRange{Int}(t.parent)
+#Base.LinearIndices(t::Tensor) = LinearIndices(t.parent)
+function Base.similar(t::Tensor, eltype::Type, axes::NTuple{N,TAxis{Int}}) where {N}
+    x = mapreduce(|, 1:N, init=UInt(0)) do i
+        index_pos(axes[i]).x << (i-1)
+    end
+    ipos = IndexPos{N}(x)
+    paren = similar(t.parent, eltype, map(parent, axes))
+    ipos(paren)
+end
+Base.similar(t::Tensor, eltype::Type, axes::Tuple{}) = T""(similar(t.parent, eltype, ()))
