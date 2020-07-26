d935e322f2d6b193022f0d10d972ab3ece9652f4
Simeon Schaub <simeondavidschaub99@gmail.com>
2020-07-27 00:17:18 +0200

    improve printing and functionality for tbegin/tend

--- src/tbeginend.jl
+++ src/tbeginend.jl
@@ -1,5 +1,3 @@
-using LegibleLambdas
-
 ###
 ### tbegin/tend
 ###
@@ -7,33 +5,46 @@ using LegibleLambdas
 struct TBeginEnd{F}
     "maps `(firstindex(::Tensor, d), lastindex(::Tensor, d))` to an index"
     f::F
+    ast
 end
 
 for (tend, _tend, l) in [(:tbegin, :_tbegin, :f), (:tend, :_tend, :l)]
     @eval begin
         $_tend(f, l) = $l
-        const $tend = TBeginEnd($_tend)
-
-        Base.show(io::IO, ::TBeginEnd{typeof($_tend)}) = print(io, $(string(tend)))
+        const $tend = TBeginEnd($_tend, $(Meta.quot(tend)))
     end
 end
 
-function Base.show(io::IO, tend::TBeginEnd{<:LegibleLambda})
-    show(io, TBeginEnd)
-    show(io, tend.f)
+Base.show(io::IO, tend::TBeginEnd) = print(io, tend.ast)
+
+macro _tbeginend(ex)
+    @assert Meta.isexpr(ex, :call)
+    showexpr = Expr(
+        :call,
+        ex.args[1],
+        [
+            if Meta.isexpr(i, :call) && Meta.isexpr(i.args[1], :.)
+                Expr(:$, Expr(:., i.args[1].args[1], Meta.quot(:ast)))
+            else
+                Expr(:$, i)
+            end
+            for i in ex.args[2:end]
+        ]...,
+    )
+    :(TBeginEnd((f, l) -> $ex, $(Meta.quot(showexpr))))
 end
 
-for f in [:(:), :+, :-]
+for f in [:(:), :+, :-, :*, :min, :max, :%, :mod, :mod1, :div, :fld]
     @eval begin
-        Base.$f(x::TBeginEnd, y::Int) = TBeginEnd(@λ (f, l) -> $f(x.f(f, l), y))
-        Base.$f(x::Int, y::TBeginEnd) = TBeginEnd(@λ (f, l) -> $f(x, y.f(f, l)))
-        Base.$f(x::TBeginEnd, y::TBeginEnd) = TBeginEnd(@λ (f, l) -> $f(x.f(f, l), y.f(f, l)))
+        Base.$f(x::TBeginEnd, y::Int) = @_tbeginend $f(x.f(f, l), y)
+        Base.$f(x::Int, y::TBeginEnd) = @_tbeginend $f(x, y.f(f, l))
+        Base.$f(x::TBeginEnd, y::TBeginEnd) = @_tbeginend $f(x.f(f, l), y.f(f, l))
     end
 end
 
-Base.:(:)(x::TBeginEnd, s::Int, y::Int) = TBeginEnd(@λ (f, l) -> x.f(f, l):s:y)
-Base.:(:)(x::Int, s::Int, y::TBeginEnd) = TBeginEnd(@λ (f, l) -> x:s:y.f(f, l))
-Base.:(:)(x::TBeginEnd, s::Int, y::TBeginEnd) = TBeginEnd(@λ (f, l) -> x.f(f, l):s:y.f(f, l))
+Base.:(:)(x::TBeginEnd, s::Int, y::Int) = @_tbeginend x.f(f, l):s:y
+Base.:(:)(x::Int, s::Int, y::TBeginEnd) = @_tbeginend x:s:y.f(f, l)
+Base.:(:)(x::TBeginEnd, s::Int, y::TBeginEnd) = @_tbeginend x.f(f, l):s:y.f(f, l)
 
 
 Base.to_index(i::TBeginEnd) = i
